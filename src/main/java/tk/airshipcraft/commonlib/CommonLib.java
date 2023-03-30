23bcc0cbb1dd646a9c7799de824d3503eb427539
zeyad rashed <brwnboy2002@gmail.com>
2023-03-30 00:22:31 -0400

    init proj

---
+++ src/main/java/tk/airshipcraft/commonlib/CommonLib.java
@@ -0,0 +1,19 @@
+package tk.airshipcraft.commonlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class file, plugins should extend this.
+ */
+public final class CommonLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
