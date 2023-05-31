70453992df95909a228641a87ba0414b55e9c1ae
Locutusque <sebby0927@gmail.com>
2023-05-31 13:12:48 -0700

    attempt to fix enable issue

--- src/main/java/tk/airshipcraft/commonlib/CommonLib.java
+++ src/main/java/tk/airshipcraft/commonlib/CommonLib.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import tk.airshipcraft.commonlib.Events.GuiClickEvent;
 import tk.airshipcraft.commonlib.Events.HologramClickListener;
 import tk.airshipcraft.commonlib.Events.InventoryClickListener;
+import tk.airshipcraft.commonlib.utils.ACRPlugin;
 import tk.airshipcraft.commonlib.utils.UiDesigner;
 
 import java.io.File;
@@ -23,11 +24,13 @@ public class CommonLib extends JavaPlugin {
         mainInstance = this;
         getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
         getServer().getPluginManager().registerEvents(new HologramClickListener(), this);
+        ACRPlugin.enableSubclasses();
     }
 
 
     @Override
     public final void onDisable() {
+        ACRPlugin.disableSubclasses();
     }
 }
 
