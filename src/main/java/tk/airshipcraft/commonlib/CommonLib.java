8c1b179d6426f8c3c7660baea14c390611d6ecf2
Locutusque <sebby0927@gmail.com>
2023-05-31 14:39:33 -0700

    attempt to fix enable issue

--- src/main/java/tk/airshipcraft/commonlib/CommonLib.java
+++ src/main/java/tk/airshipcraft/commonlib/CommonLib.java
@@ -1,19 +1,9 @@
 package tk.airshipcraft.commonlib;
 
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import tk.airshipcraft.commonlib.Events.GuiClickEvent;
 import tk.airshipcraft.commonlib.Events.HologramClickListener;
 import tk.airshipcraft.commonlib.Events.InventoryClickListener;
 import tk.airshipcraft.commonlib.utils.ACRPlugin;
-import tk.airshipcraft.commonlib.utils.ACRPluginManager;
-import tk.airshipcraft.commonlib.utils.UiDesigner;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Supplier;
 
 /**
  * Main class file, plugins should extend this.
@@ -25,13 +15,13 @@ public class CommonLib extends JavaPlugin {
         mainInstance = this;
         getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
         getServer().getPluginManager().registerEvents(new HologramClickListener(), this);
-        ACRPluginManager.enableSubclasses();
+        ACRPlugin.enableSubclasses();
     }
 
 
     @Override
     public final void onDisable() {
-        ACRPluginManager.disableSubclasses();
+        ACRPlugin.disableSubclasses();
     }
 }
 
--- src/main/java/tk/airshipcraft/commonlib/utils/ACRPlugin.java
+++ src/main/java/tk/airshipcraft/commonlib/utils/ACRPlugin.java
@@ -1,8 +1,73 @@
 package tk.airshipcraft.commonlib.utils;
 
+import tk.airshipcraft.commonlib.CommonLib;
 
-public interface ACRPlugin {
-    void onPluginEnable();
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-    void onPluginDisable();
+public abstract class ACRPlugin extends CommonLib {
+    private static List<ACRPlugin> plugins = new ArrayList<>();
+
+    public ACRPlugin() {
+    }
+
+    public static void enableSubclasses() {
+        List<Class<? extends ACRPlugin>> subclasses = getSubclassesOf(ACRPlugin.class);
+        for (Class<? extends ACRPlugin> subclass : subclasses) {
+            try {
+                ACRPlugin plugin = subclass.getDeclaredConstructor().newInstance();
+                plugins.add(plugin);
+                plugin.onPluginEnable(); // Call onPluginEnable() for each enabled plugin
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void disableSubclasses() {
+        for (int i = plugins.size() - 1; i >= 0; i--) {
+            ACRPlugin plugin = plugins.get(i);
+            if (plugin.isEnabled()) {
+                plugin.onPluginDisable();
+                plugins.remove(i);
+            }
+        }
+    }
+
+    public static <T> List<Class<? extends T>> getSubclassesOf(Class<T> clazz) {
+        List<Class<? extends T>> subclasses = new ArrayList<>();
+        Package classPackage = clazz.getPackage();
+        if (classPackage != null) {
+            String packageName = classPackage.getName();
+            String packagePath = packageName.replace('.', '/');
+            URL packageURL = clazz.getClassLoader().getResource(packagePath);
+            if (packageURL != null) {
+                File packageDirectory = new File(packageURL.getFile());
+                String[] classNames = packageDirectory.list();
+                if (classNames != null) {
+                    for (String className : classNames) {
+                        if (className.endsWith(".class")) {
+                            try {
+                                String fullClassName = packageName + '.' + className.substring(0, className.length() - 6);
+                                Class<?> c = Class.forName(fullClassName);
+                                if (clazz.isAssignableFrom(c) && !clazz.equals(c)) {
+                                    System.out.println(clazz);
+                                    subclasses.add(c.asSubclass(clazz));
+                                }
+                            } catch (ClassNotFoundException e) {
+                                throw new NullPointerException("Class not found: " + e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return subclasses;
+    }
+
+    public abstract void onPluginEnable();
+
+    public abstract void onPluginDisable();
 }
