b760f8b6a9e37cde4ad61b50f3e6db917861fb86
zeyad rashed <brwnboy2002@gmail.com>
2023-11-13 14:43:38 -0500

    fix: make commonlib class be a singleton pattern

--- src/main/java/tk/airshipcraft/commonlib/CommonLib.java
+++ src/main/java/tk/airshipcraft/commonlib/CommonLib.java
@@ -6,22 +6,40 @@ import tk.airshipcraft.commonlib.Events.InventoryClickListener;
 import tk.airshipcraft.commonlib.utils.ACRPlugin;
 
 /**
- * Main class file, plugins should extend this.
+ * Main class of the CommonLib library.
+ * This class initializes the library and manages the lifecycle of the plugins that extend it.
+ * It uses a Singleton pattern to ensure that only one instance of CommonLib exists.
  */
 public class CommonLib extends JavaPlugin {
-    public static CommonLib mainInstance;
+    private static CommonLib instance;
 
+    /**
+     * Returns the single instance of CommonLib.
+     *
+     * @return The singleton instance of CommonLib.
+     */
+    public static CommonLib getInstance() {
+        return instance;
+    }
+
+    /**
+     * Initializes the library and registers event listeners.
+     * This method is called by the Bukkit framework when the plugin is enabled.
+     */
     @Override
-    public final void onEnable() {
-        mainInstance = this;
+    public void onEnable() {
+        instance = this;
         getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
         getServer().getPluginManager().registerEvents(new HologramClickListener(), this);
         ACRPlugin.enableSubclasses();
     }
 
-
+    /**
+     * Cleans up resources and disables the plugins that extend this library.
+     * This method is called by the Bukkit framework when the plugin is disabled.
+     */
     @Override
-    public final void onDisable() {
+    public void onDisable() {
         ACRPlugin.disableSubclasses();
     }
 }
