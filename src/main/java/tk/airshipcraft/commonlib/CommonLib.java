4d1c5d32aa3232fac251b03a40d248e93ffa8d03
zeyad rashed <brwnboy2002@gmail.com>
2023-11-13 14:47:33 -0500

    feat: add steps to set up logging

--- src/main/java/tk/airshipcraft/commonlib/CommonLib.java
+++ src/main/java/tk/airshipcraft/commonlib/CommonLib.java
@@ -5,13 +5,18 @@ import tk.airshipcraft.commonlib.Events.HologramClickListener;
 import tk.airshipcraft.commonlib.Events.InventoryClickListener;
 import tk.airshipcraft.commonlib.utils.ACRPlugin;
 
+import java.util.logging.Level;
+
 /**
  * Main class of the CommonLib library.
  * This class initializes the library and manages the lifecycle of the plugins that extend it.
  * It uses a Singleton pattern to ensure that only one instance of CommonLib exists.
+ * It also provides logging capabilities.
  */
 public class CommonLib extends JavaPlugin {
+
     private static CommonLib instance;
+    private boolean debugEnabled = false; // Add a flag to control debug logging
 
     /**
      * Returns the single instance of CommonLib.
@@ -23,15 +28,31 @@ public class CommonLib extends JavaPlugin {
     }
 
     /**
-     * Initializes the library and registers event listeners.
+     * Initializes the library, sets up logging, and registers event listeners.
      * This method is called by the Bukkit framework when the plugin is enabled.
      */
     @Override
     public void onEnable() {
         instance = this;
+        setupLogging(); // Setup logging
         getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
         getServer().getPluginManager().registerEvents(new HologramClickListener(), this);
         ACRPlugin.enableSubclasses();
+    }
+
+    /**
+     * Sets up the logging mechanism.
+     * This method can be used to configure logging and read the debug setting.
+     */
+    private void setupLogging() {
+        // You can read the debug setting from a config file or set it programmatically
+        // For example, you can uncomment the following line to enable debug logging:
+        // debugEnabled = true;
+
+        logInfo("CommonLib is enabled");
+        if (debugEnabled) {
+            logDebug("Debug mode is enabled");
+        }
     }
 
     /**
@@ -40,6 +61,29 @@ public class CommonLib extends JavaPlugin {
      */
     @Override
     public void onDisable() {
+        logInfo("CommonLib is being disabled");
         ACRPlugin.disableSubclasses();
     }
+
+    /**
+     * Logs an informational message.
+     *
+     * @param message The message to log.
+     */
+    public void logInfo(String message) {
+        getLogger().info(message);
+    }
+
+    /**
+     * Logs a debug message. Debug messages are only logged if debug mode is enabled.
+     *
+     * @param message The debug message to log.
+     */
+    public void logDebug(String message) {
+        if (debugEnabled) {
+            getLogger().log(Level.FINE, message);
+        }
+    }
+
+    // Additional logging methods like logWarning, logError, etc., can be added similarly.
 }
