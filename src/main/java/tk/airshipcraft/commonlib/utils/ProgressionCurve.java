8589eaf137e6b023cb290f2f8523bea84680d240
zeyad rashed <brwnboy2002@gmail.com>
2023-11-16 15:03:10 -0500

    feat: create a progression curve algorithim
    This class represents a progression curve for a game where players earn tokens and rank up. The curve is designed to slow down more active players, allowing newer or less active players a better chance to catch up.
    
    R(T)= ceil[1+(C/T)]

---
+++ src/main/java/tk/airshipcraft/commonlib/utils/ProgressionCurve.java
@@ -0,0 +1,28 @@
+package tk.airshipcraft.commonlib.utils;
+
+/**
+ * This class represents a progression curve for a game where players earn tokens and rank up.
+ * The curve is designed to slow down more active players, allowing newer or less active
+ * players a better chance to catch up.
+ *
+ * @author notzune
+ * @since 2023-11-16
+ * @version 1.0.0
+ */
+public class ProgressionCurve {
+
+    /**
+     * Calculates the number of rank-ups required for the next token.
+     *
+     * @param tokensEarned The total number of tokens already earned by the player.
+     * @param catchUpFactor The catch-up factor that controls the rate of growth in difficulty.
+     * @return The number of rank-ups required to earn the next token, rounded up to the nearest whole number.
+     */
+    public static int calculateRankUpsForNextToken(int tokensEarned, double catchUpFactor) {
+        // Apply the progression formula as per the design
+        double rankUpsRequired = 1 + (double) tokensEarned / catchUpFactor;
+
+        // Use ceiling to always round up to the nearest whole number
+        return (int) Math.ceil(rankUpsRequired);
+    }
+}
