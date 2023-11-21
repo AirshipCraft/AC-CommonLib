1ee1b4743703acc16b61a4a752a424bf6eef496d
zeyad rashed <brwnboy2002@gmail.com>
2023-11-20 21:36:15 -0500

    docs: add javadoc comments

--- src/main/java/tk/airshipcraft/commonlib/world/RectangleArea.java
+++ src/main/java/tk/airshipcraft/commonlib/world/RectangleArea.java
@@ -8,16 +8,30 @@ import java.util.Collection;
 import java.util.HashSet;
 
 /**
- * Checks a rectangular area that is Y limited.
+ * Represents a rectangular area in a Minecraft world with Y-level boundaries.
+ * The area is defined by a center {@link Location}, a size along the X-axis (xSize),
+ * and a size along the Z-axis (zSize). This class can be used to check if specific
+ * locations or chunks fall within the area.
+ *
+ * @author notzune
+ * @version 1.0.0
+ * @since 2023-04-01
  */
 public class RectangleArea extends AbstractYLimitedArea {
 
     private Location center;
-
     private double xSize;
-
     private double zSize;
 
+    /**
+     * Constructs a new {@code RectangleArea} with specified Y boundaries, center, and size.
+     *
+     * @param lowerYBound The lower Y boundary of the area.
+     * @param upperYBound The upper Y boundary of the area.
+     * @param center      The center location of the rectangular area.
+     * @param xSize       Half of the size of the rectangle along the X-axis.
+     * @param zSize       Half of the size of the rectangle along the Z-axis.
+     */
     public RectangleArea(double lowerYBound, double upperYBound, Location center, double xSize, double zSize) {
         super(lowerYBound, upperYBound);
         this.center = center;
@@ -25,61 +39,96 @@ public class RectangleArea extends AbstractYLimitedArea {
         this.zSize = zSize;
     }
 
+    /**
+     * Retrieves a collection of chunks that are within the rectangular area.
+     *
+     * @return A collection of {@link Chunk} objects within the area.
+     */
     @Override
     public Collection<Chunk> getChunks() {
         Collection<Chunk> chunks = new HashSet<>();
+        // Iterate through the area's extent along the X and Z axes, adding each chunk to the collection.
         for (double x = center.getX() - xSize; x <= center.getX() + xSize; x += 16) {
             for (double z = center.getZ() - zSize; z <= center.getZ() + zSize; z += 16) {
                 chunks.add(new Location(center.getWorld(), x, center.getY(), z).getChunk());
             }
-            // last one might have been skipped
+            // Ensure the last chunk in the Z-axis is included.
             chunks.add(new Location(center.getWorld(), x, center.getY(), center.getZ() + zSize).getChunk());
         }
         return chunks;
     }
 
+    /**
+     * Checks if a location is within the rectangular area.
+     *
+     * @param loc The location to check.
+     * @return True if the location is within the area, false otherwise.
+     */
     @Override
     public boolean isInArea(Location loc) {
         double x = loc.getX();
         double z = loc.getZ();
-        return loc.getWorld().getUID().equals(getWorld().getUID()) && (center.getX() - xSize) <= x
-                && (center.getX() + xSize) >= x && (center.getZ() - zSize) <= z && (center.getZ() + zSize) >= z
+        return loc.getWorld().equals(getWorld())
+                && x >= center.getX() - xSize && x <= center.getX() + xSize
+                && z >= center.getZ() - zSize && z <= center.getZ() + zSize
                 && super.isInArea(loc);
     }
 
+    /**
+     * Gets the center location of the rectangular area.
+     *
+     * @return The center {@link Location} of the area.
+     */
     @Override
     public Location getCenter() {
         return center;
     }
 
+    /**
+     * Gets the world where the rectangular area is located.
+     *
+     * @return The {@link World} object representing the world of the area.
+     */
     @Override
     public World getWorld() {
         return center.getWorld();
     }
 
     /**
-     * @return Half of the diameter of this rectangle in x dimension
+     * Gets the size of the rectangular area along the X-axis.
+     *
+     * @return Half the size of the area along the X-axis.
      */
     public double getXSize() {
         return xSize;
     }
 
     /**
-     * @return Half of the diameter of this rectangle in z dimension
+     * Gets the size of the rectangular area along the Z-axis.
+     *
+     * @return Half the size of the area along the Z-axis.
      */
     public double getZSize() {
         return zSize;
     }
 
+    /**
+     * Retrieves a collection of pseudo chunks that are within the rectangular area.
+     * Pseudo chunks are represented by {@link PseudoChunk} and do not contain actual chunk data,
+     * but represent chunk locations within the area.
+     *
+     * @return A collection of {@link PseudoChunk} objects within the area.
+     */
     @Override
     public Collection<PseudoChunk> getPseudoChunks() {
         Collection<PseudoChunk> chunks = new HashSet<>();
+        // Iterate through the area's extent along the X and Z axes, adding each pseudo chunk to the collection.
         for (int x = (int) (center.getX() - xSize); x <= center.getX() + xSize; x += 16) {
             for (int z = (int) (center.getZ() - zSize); z <= center.getZ() + zSize; z += 16) {
-                chunks.add(new PseudoChunk(center.getWorld(), x / 16, z / 16));
+                chunks.add(new PseudoChunk(center.getWorld(), x >> 4, z >> 4));
             }
-            // last one might have been skipped
-            chunks.add(new PseudoChunk(center.getWorld(), x / 16, ((int) (center.getZ() + zSize)) / 16));
+            // Ensure the last pseudo chunk in the Z-axis is included.
+            chunks.add(new PseudoChunk(center.getWorld(), x >> 4, ((int) (center.getZ() + zSize)) >> 4));
         }
         return chunks;
     }
