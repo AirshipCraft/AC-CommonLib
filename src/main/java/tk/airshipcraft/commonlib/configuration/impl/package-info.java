de264a5965e060df544da3fc68db95d1f3d74c5a
zeyad rashed <brwnboy2002@gmail.com>
2023-11-23 00:05:07 -0500

    docs: add package-infos

---
+++ src/main/java/tk/airshipcraft/commonlib/configuration/impl/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Implementation subpackage for configuration management, containing concrete classes and
+ * annotations used for managing player preferences and settings within the plugin.
+ *
+ <h2>Classes:</h2>
+ * <ul>
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.impl.IPlayerPreference} - An interface defining
+ *   the necessary methods for a player preference class, including load, save, and describe methods.</li>
+ *
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.impl.PlayerPref} - An annotation used to mark
+ *   fields in IPlayerPreference implementations that should be persisted and loaded from the data store.</li>
+ * </ul>
+ */
+package tk.airshipcraft.commonlib.configuration.impl;
---
+++ src/main/java/tk/airshipcraft/commonlib/gui/objects/impl/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides implementation details for the objects in the GUI system, such as teams which can
+ * be utilized within different UI components for grouping and organization.
+ *
+ * <h2>Classes:</h2>
+ * <ul>
+ *   <li>{@link tk.airshipcraft.commonlib.gui.objects.impl.ITeam} - Allows different plugins to create and manage their own team logic, structures, and hierarchies.</li>
+ * </ul>
+ */
+package tk.airshipcraft.commonlib.gui.objects.impl;
