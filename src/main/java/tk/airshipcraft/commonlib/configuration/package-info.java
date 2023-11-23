7dd7fad49aea88b5729a2302b2c5c8febadaee1f
zeyad rashed <brwnboy2002@gmail.com>
2023-11-22 22:45:54 -0500

    style: formatting fix

--- src/main/java/tk/airshipcraft/commonlib/configuration/package-info.java
+++ src/main/java/tk/airshipcraft/commonlib/configuration/package-info.java
@@ -1,23 +1,22 @@
 /**
- * Provides the classes necessary for managing player preferences within a Bukkit plugin.
+ * <p>Provides the classes necessary for managing player preferences within a Bukkit plugin.
  * The preference system allows for persistent storage and retrieval of player-specific settings,
- * which can be used across different plugins.
- * <p>
- * Classes:
- * <p>
- * {@link tk.airshipcraft.commonlib.configuration.impl.IPlayerPreference} - An interface defining
- * the necessary methods for a player preference class, including load, save, and describe methods.
- * </p>
- * <p>
- * {@link tk.airshipcraft.commonlib.configuration.impl.PlayerPref} - An annotation used to mark
- * fields in IPlayerPreference implementations that should be persisted and loaded from the data store.
- * </p>
- * <p>
- * {@link tk.airshipcraft.commonlib.configuration.PlayerPreference} - An abstract class that
- * provides basic implementations of IPlayerPreference methods and handles the reflection logic
- * for annotated fields.
- * <p>
- * Usage example in another plugin:
+ * which can be used across different plugins.</p>
+ *
+ * <h2>Classes:</h2>
+ * <ul>
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.impl.IPlayerPreference} - An interface defining
+ *   the necessary methods for a player preference class, including load, save, and describe methods.</li>
+ *
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.impl.PlayerPref} - An annotation used to mark
+ *   fields in IPlayerPreference implementations that should be persisted and loaded from the data store.</li>
+ *
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.PlayerPreference} - An abstract class that
+ *   provides basic implementations of IPlayerPreference methods and handles the reflection logic
+ *   for annotated fields.</li>
+ * </ul>
+ *
+ * <h2>Usage example in another plugin:</h2>
  * <pre>{@code
  * public class MyPluginPlayerPreferences implements IPlayerPreference {
  *
@@ -51,26 +50,25 @@
  *     }
  * }
  * }</pre>
- * </p>
- * <p>
- * {@link tk.airshipcraft.commonlib.configuration.PreferenceProcessor} - A utility class that
- * contains static methods to load and save annotated fields from and to both a file configuration
- * and a MySQL database using reflection.
- * </p>
- * <p>
- * {@link tk.airshipcraft.commonlib.configuration.PreferencesManager} - Manages all player preferences,
- * providing methods to register, load, save, and reset preferences, as well as to describe them for
- * individual players.
- * </p>
- * <p>
- * Usage:
- * To use the preference system, a plugin developer must create a class that implements the
+ *
+ * <ul>
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.PreferenceProcessor} - A utility class that
+ *   contains static methods to load and save annotated fields from and to both a file configuration
+ *   and a MySQL database using reflection.</li>
+ *
+ *   <li>{@link tk.airshipcraft.commonlib.configuration.PreferencesManager} - Manages all player preferences,
+ *   providing methods to register, load, save, and reset preferences, as well as to describe them for
+ *   individual players.</li>
+ * </ul>
+ *
+ * <h2>Usage:</h2>
+ * <p>To use the preference system, a plugin developer must create a class that implements the
  * {@link tk.airshipcraft.commonlib.configuration.impl.IPlayerPreference} interface and use the
  * {@link tk.airshipcraft.commonlib.configuration.impl.PlayerPref} annotation to mark which fields
  * should be managed. The {@link tk.airshipcraft.commonlib.configuration.PreferencesManager} is then
- * used to handle these preferences.
- * <p>
- * Example:
+ * used to handle these preferences.</p>
+ *
+ * <h2>Example:</h2>
  * <pre>{@code
  * public class MyPluginPreference implements IPlayerPreference {
  *     @PlayerPref
@@ -93,14 +91,13 @@
  * PreferencesManager prefManager = new PreferencesManager();
  * prefManager.registerPreference(player.getUniqueId(), new MyPluginPreference());
  * }</pre>
- * </p>
- * The {@link tk.airshipcraft.commonlib.commands.PreferenceCommand} can be used as a template for
+ *
+ * <p>The {@link tk.airshipcraft.commonlib.commands.PreferenceCommand} can be used as a template for
  * creating commands that interact with the player preferences, allowing players to set and view
- * their preferences in-game.
- * <p>
- * Please note that the reflection-based operations require careful exception handling and validation
- * to ensure data integrity and plugin stability.
- * </p>
+ * their preferences in-game.</p>
+ *
+ * <p>Please note that the reflection-based operations require careful exception handling and validation
+ * to ensure data integrity and plugin stability.</p>
  *
  * @author notzune
  * @version 1.0.0
