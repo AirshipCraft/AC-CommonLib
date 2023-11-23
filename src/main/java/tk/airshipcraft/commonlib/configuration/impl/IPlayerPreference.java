d00632cd1cd9493224ee4657f27fc38cddce145c
zeyad rashed <brwnboy2002@gmail.com>
2023-11-23 00:12:41 -0500

    style: style changes

--- src/main/java/tk/airshipcraft/commonlib/configuration/impl/IPlayerPreference.java
+++ src/main/java/tk/airshipcraft/commonlib/configuration/impl/IPlayerPreference.java
@@ -3,9 +3,9 @@ package tk.airshipcraft.commonlib.configuration.impl;
 import org.bukkit.entity.Player;
 
 /**
- * Interface for player preference handling.
- * Classes that implement this interface are responsible for managing the loading,
- * saving, and resetting of player-specific preferences.
+ * IPlayerPreference is an interface that defines the contract for managing player-specific preferences in a Minecraft server environment.
+ * Implementations of this interface should handle the loading, saving, and resetting of preferences for individual players.
+ * This interface is essential for ensuring consistent behavior across different implementations of player preference management.
  *
  * @author notzune
  * @version 1.0.0
@@ -14,35 +14,39 @@ import org.bukkit.entity.Player;
 public interface IPlayerPreference {
 
     /**
-     * Loads the player's preferences.
-     * Implementations should retrieve and apply preference values from a persistent store.
+     * Loads the player's preferences from a persistent data store.
+     * This method should be implemented to retrieve the stored preferences of a player and apply them.
+     * It's typically called when a player joins the server or a reload of preferences is needed.
      *
      * @param player The player whose preferences are to be loaded.
      */
     void load(Player player);
 
     /**
-     * Saves the player's preferences.
-     * Implementations should persist the current state of preferences to a store.
+     * Saves the current state of the player's preferences to a persistent data store.
+     * Implementations should ensure that any changes made to the preferences are persisted.
+     * This method is usually called when a player leaves the server or when preferences are changed.
      *
      * @param player The player whose preferences are to be saved.
      */
     void save(Player player);
 
     /**
-     * Resets the player's preferences to default values.
-     * Implementations should revert preferences back to a default state.
+     * Resets the player's preferences to their default values.
+     * This method is useful for situations where a player's preferences need to be cleared or reverted.
+     * Implementations should ensure that all preferences are set to their initial state.
      *
      * @param player The player whose preferences are to be reset.
      */
     void reset(Player player);
 
     /**
-     * Gets a summary or detailed description of the player's preferences.
-     * This can be used for informative purposes or for debugging.
+     * Provides a string representation of the player's current preferences.
+     * This can be used to display the preferences to the player, for logging, or for administrative purposes.
+     * The format and detail level of the description can vary depending on the implementation.
      *
      * @param player The player whose preference summary is to be retrieved.
-     * @return A string representation of the player's preferences.
+     * @return A string describing the player's current preferences. The format of this string is implementation-specific.
      */
     String describe(Player player);
 }
