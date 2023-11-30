88337c1307bd892065fe993e29eea22b1c3e441f
zeyad rashed <brwnboy2002@gmail.com>
2023-11-29 21:06:33 -0500

    docs: add javadoc comments to blockutils

--- src/main/java/tk/airshipcraft/commonlib/utils/BlockUtils.java
+++ src/main/java/tk/airshipcraft/commonlib/utils/BlockUtils.java
@@ -6,11 +6,20 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Utility class for categorizing different types of Minecraft blocks.
+ * Allows for easy identification of block types such as logs, stones, planks, etc.
+ *
+ * @author notzune
+ * @version 1.0.0
+ * @since 2023-11-28
+ */
 public class BlockUtils {
 
     private static final Map<Material, BlockType> blockTypeMap = new HashMap<>();
     private static final Map<BlockType, Set<Material>> typeToMaterialsMap = new HashMap<>();
 
+    // Static initializer to populate the block type mappings
     static {
         // Initialize the mapping
         for (Material material : Material.values()) {
@@ -32,6 +41,12 @@ public class BlockUtils {
         }
     }
 
+    /**
+     * Adds a mapping between a material and a block type.
+     *
+     * @param material The material to be mapped.
+     * @param type The block type to associate with the material.
+     */
     private static void addMapping(Material material, BlockType type) {
         blockTypeMap.put(material, type);
 
@@ -39,6 +54,12 @@ public class BlockUtils {
         typeToMaterialsMap.get(type).add(material);
     }
 
+    /**
+     * Checks if a material is an unstripped log type.
+     *
+     * @param material The material to check.
+     * @return true if the material is an unstripped log, false otherwise.
+     */
     private static boolean isUnstrippedLogMaterial(Material material) {
         return material == Material.ACACIA_LOG ||
                 material == Material.OAK_LOG ||
@@ -51,6 +72,12 @@ public class BlockUtils {
                 material == Material.CRIMSON_STEM;
     }
 
+    /**
+     * Checks if a material is an stripped log type.
+     *
+     * @param material The material to check.
+     * @return true if the material is an stripped log, false otherwise.
+     */
     private static boolean isStrippedLogMaterial(Material material) {
         return material == Material.STRIPPED_ACACIA_LOG ||
                 material == Material.STRIPPED_OAK_LOG ||
@@ -63,6 +90,12 @@ public class BlockUtils {
                 material == Material.STRIPPED_CRIMSON_STEM;
     }
 
+    /**
+     * Checks if a material is a log type (agnostic if it is stripped or unstripped).
+     *
+     * @param material The material to check.
+     * @return true if the material is a log, false otherwise.
+     */
     private static boolean isLogMaterial(Material material) {
         return material == Material.ACACIA_LOG || material == Material.STRIPPED_ACACIA_LOG ||
                 material == Material.OAK_LOG || material == Material.STRIPPED_OAK_LOG ||
@@ -92,35 +125,81 @@ public class BlockUtils {
                 material == Material.MANGROVE_PLANKS;
     }
 
+    /**
+     * Retrieves the block type of a given material.
+     *
+     * @param material The material whose block type is to be determined.
+     * @return The block type of the material, or null if the material does not have a mapped block type.
+     */
     public static BlockType getBlockType(Material material) {
         return blockTypeMap.get(material);
     }
 
+
+    /**
+     * Retrieves all materials associated with a given block type.
+     *
+     * @param type The block type for which materials are to be retrieved.
+     * @return A set of materials associated with the specified block type.
+     */
     public static Set<Material> getMaterialsFromType(BlockType type) {
         return typeToMaterialsMap.getOrDefault(type, new HashSet<>());
     }
 
     // Added boolean check methods
+    /**
+     * Checks if a material is a log (including both stripped and unstripped variants).
+     *
+     * @param material The material to check.
+     * @return true if the material is any type of log, false otherwise.
+     */
     public static boolean isLog(Material material) {
         return getBlockType(material) == BlockType.LOG;
     }
 
-    public static boolean isStone(Material material) {
-        return getBlockType(material) == BlockType.STONE;
-    }
-
-    public static boolean isPlank(Material material) {
-        return getBlockType(material) == BlockType.PLANK;
-    }
-
+    /**
+     * Checks if a material is an unstripped log.
+     *
+     * @param material The material to check.
+     * @return true if the material is any type of unstripped log, false otherwise.
+     */
     public static boolean isLogUnstripped(Material material) {
         return getBlockType(material) == BlockType.LOG_UNSTRIPPED;
     }
 
+    /**
+     * Checks if a material is an stripped log.
+     *
+     * @param material The material to check.
+     * @return true if the material is any type of stripped log, false otherwise.
+     */
     public static boolean isLogStripped(Material material) {
         return getBlockType(material) == BlockType.LOG_STRIPPED;
     }
 
+    /**
+     * Checks if a material is a stone.
+     *
+     * @param material The material to check.
+     * @return true if the material is any type of stone, false otherwise.
+     */
+    public static boolean isStone(Material material) {
+        return getBlockType(material) == BlockType.STONE;
+    }
+
+    /**
+     * Checks if a material is a plank.
+     *
+     * @param material The material to check.
+     * @return true if the material is any type of plank, false otherwise.
+     */
+    public static boolean isPlank(Material material) {
+        return getBlockType(material) == BlockType.PLANK;
+    }
+
+    /**
+     * Enum representing different types of Minecraft blocks grouped by category.
+     */
     public enum BlockType {
         LOG, LOG_STRIPPED, LOG_UNSTRIPPED,
         WOOD, PLANK,
