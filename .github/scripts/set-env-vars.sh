ef05d9cb1ead252b3a6ee303c11272dcd4bd8f2a
zeyad rashed <brwnboy2002@gmail.com>
2023-12-24 21:48:02 -0500

    ci: update scripts

--- .github/scripts/set-env-vars.sh
+++ .github/scripts/set-env-vars.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# This script would be responsible for echoing the environment variable
-# declarations to $GITHUB_ENV.
-
 echo "DEFAULT_BRANCH=main" >> $GITHUB_ENV
 echo "NODE_VERSION=20.x" >> $GITHUB_ENV
 echo "PYTHON_VERSION=3.9" >> $GITHUB_ENV
 echo "JAVA_VERSION=17" >> $GITHUB_ENV
-
-# ... any additional variables ...
