4065a223b556c5e862876489f0b4375e4cd92c17
zeyad rashed <brwnboy2002@gmail.com>
2023-12-27 00:48:48 -0500

    docs: update package-info

--- src/main/java/tk/airshipcraft/commonlib/calendar/package-info.java
+++ src/main/java/tk/airshipcraft/commonlib/calendar/package-info.java
@@ -1,29 +1,45 @@
 /**
  * <h1>AirshipCraft Calendar and Event System Package</h1>
  * <p>
- * This package contains classes for the AirshipCraft calendar and event system in Minecraft.
- * It includes managers for handling the in-game calendar, seasons, events, and news generation,
- * as well as auxiliary and utility classes for supporting functionalities like AI-driven NPCs,
- * dynamic weather, and a player interface.
+ * This package, part of the AirshipCraft's <code>CommonLib</code> core library, provides a calendar and event system
+ * for Minecraft servers. Designed to serve as an API, it allows other plugins to integrate time-based functionalities
+ * such as seasonal changes, event scheduling, and dynamic in-game news. The system is built for ease of use and
+ * interoperability with various server features, including agriculture, economy, and AI-driven NPCs.
  * </p>
  *
  * <h2>Core Classes:</h2>
  * <ul>
- *   <li><b>CalendarManager:</b> Manages the in-game calendar, time conversion, and special calendar events.</li>
- *   <li><b>SeasonManager:</b> Handles seasonal changes and environmental effects related to seasons.</li>
- *   <li><b>EventManager:</b> Manages in-game events linked to the calendar, including scheduling and triggering.</li>
- *   <li><b>NewsGenerator:</b> Generates news and newsletters for player updates and server events.</li>
+ *   <li><b>CalendarManager:</b> Manages the in-game calendar, time conversion, and special calendar events.
+ *       Exposes API methods for other plugins to access current date and time information.</li>
+ *   <li><b>SeasonManager:</b> Handles seasonal changes and environmental effects related to seasons.
+ *       Provides an API for querying the current season, supporting use cases like crop growth management in an agriculture plugin.</li>
+ *   <li><b>EventManager:</b> Manages in-game events linked to the calendar, including scheduling and triggering.
+ *       Features an API for other plugins to hook into event-related functionalities and create dynamic server events.</li>
+ *   <li><b>NewsGenerator:</b> Generates news and newsletters for player updates and server events.
+ *       Integrates with the calendar system to offer news generation services to other plugins.</li>
  * </ul>
  *
  * <h2>Auxiliary Classes:</h2>
  * <ul>
- *   <li><b>DatabaseManager:</b> Handles data storage and retrieval for player and event data.</li>
- *   <li><b>UserInterfaceManager:</b> Manages player interfaces for calendar and event interactions.</li>
+ *   <li><b>DatabaseManager:</b> Manages data storage and retrieval for player and event data.
+ *       Essential for maintaining a consistent and persistent calendar state across server sessions.</li>
+ *   <li><b>UserInterfaceManager:</b> Provides an interface for players to interact with the calendar and event systems.
+ *       Ensures intuitive and accessible interactions for players with calendar-related information.</li>
+ * </ul>
+ *
+ * <h2>Example Use Cases:</h2>
+ * <ul>
+ *   <li><b>Agriculture Plugin:</b> Utilizes <code>SeasonManager</code> to dynamically adjust crop growth rates and harvesting schedules
+ *       based on the current season, enhancing realism and gameplay depth.</li>
+ *   <li><b>Economy Plugin:</b> Integrates with <code>EventManager</code> to create and manage market events or special sales
+ *       that are tied to in-game holidays, seasons, or other calendar events.</li>
+ *   <li><b>NPC Interaction Plugin:</b> Leverages <code>CalendarManager</code> to modify NPC behaviors or available quests
+ *       based on the time of year, special events, or seasonal changes, contributing to a dynamic game environment.</li>
  * </ul>
  *
  * <p>
- * This structure provides a comprehensive framework for the AirshipCraft server's calendar and
- * event system, aimed at enhancing the immersive experience through dynamic time and event management.
+ * This package, as a component of <code>commonlib</code>, provides a comprehensive and reusable calendar system,
+ * enhancing the capabilities of other plugins and contributing to a more dynamic, immersive Minecraft server environment.
  * </p>
  *
  * @author notzune
