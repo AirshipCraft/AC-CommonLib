f90bece732e55ecb8f759f6b3445190646e43cac
zeyad rashed <brwnboy2002@gmail.com>
2023-12-27 00:44:08 -0500

    docs: added package info

--- src/main/java/tk/airshipcraft/commonlib/calendar/package-info.java
+++ src/main/java/tk/airshipcraft/commonlib/calendar/package-info.java
@@ -1,1 +1,33 @@
+/**
+ * <h1>AirshipCraft Calendar and Event System Package</h1>
+ * <p>
+ * This package contains classes for the AirshipCraft calendar and event system in Minecraft.
+ * It includes managers for handling the in-game calendar, seasons, events, and news generation,
+ * as well as auxiliary and utility classes for supporting functionalities like AI-driven NPCs,
+ * dynamic weather, and a player interface.
+ * </p>
+ *
+ * <h2>Core Classes:</h2>
+ * <ul>
+ *   <li><b>CalendarManager:</b> Manages the in-game calendar, time conversion, and special calendar events.</li>
+ *   <li><b>SeasonManager:</b> Handles seasonal changes and environmental effects related to seasons.</li>
+ *   <li><b>EventManager:</b> Manages in-game events linked to the calendar, including scheduling and triggering.</li>
+ *   <li><b>NewsGenerator:</b> Generates news and newsletters for player updates and server events.</li>
+ * </ul>
+ *
+ * <h2>Auxiliary Classes:</h2>
+ * <ul>
+ *   <li><b>DatabaseManager:</b> Handles data storage and retrieval for player and event data.</li>
+ *   <li><b>UserInterfaceManager:</b> Manages player interfaces for calendar and event interactions.</li>
+ * </ul>
+ *
+ * <p>
+ * This structure provides a comprehensive framework for the AirshipCraft server's calendar and
+ * event system, aimed at enhancing the immersive experience through dynamic time and event management.
+ * </p>
+ *
+ * @author notzune
+ * @version 1.0.0
+ * @since 2023-12-27
+ */
 package tk.airshipcraft.commonlib.calendar;
