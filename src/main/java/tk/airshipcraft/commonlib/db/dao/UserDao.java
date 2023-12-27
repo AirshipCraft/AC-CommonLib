0d1876ef5354b7a60c75e7bfd8bd00a78856e976
zeyad rashed <brwnboy2002@gmail.com>
2023-12-27 13:28:21 -0500

    feat: create user dao interface

---
+++ src/main/java/tk/airshipcraft/commonlib/db/dao/UserDao.java
@@ -0,0 +1,14 @@
+package tk.airshipcraft.commonlib.db.dao;
+
+
+import tk.airshipcraft.commonlib.db.GenericDao;
+import tk.airshipcraft.commonlib.db.model.User;
+
+import java.util.UUID;
+
+/**
+ * Interface for data access operations on users.
+ */
+public interface UserDao extends GenericDao<User, UUID> {
+    // Additional user-specific methods can be defined here
+}
