0d5fed47a45d9563da2b4d86ddb7e6463ac7d1e8
zeyad rashed <brwnboy2002@gmail.com>
2024-01-05 01:47:54 -0500

    fix: make fields optional in AbstractGameEvent

--- src/main/java/tk/airshipcraft/commonlib/calendar/impl/AbstractGameEvent.java
+++ src/main/java/tk/airshipcraft/commonlib/calendar/impl/AbstractGameEvent.java
@@ -3,25 +3,41 @@ package tk.airshipcraft.commonlib.calendar.impl;
 import org.bukkit.Bukkit;
 import tk.airshipcraft.commonlib.calendar.IGameEvent;
 
+import java.util.Optional;
+
+/**
+ * The AbstractGameEvent abstract class provides a structure for creating different types of in-game events.
+ * Subclasses of AbstractGameEvent can define specific behaviors in their trigger method.
+ *
+ * @author notzune
+ * @version 1.0.0
+ * @since 2024-01-04
+ */
 public abstract class AbstractGameEvent implements IGameEvent {
 
-    private String command;
-    private String message;
+    private final Optional<String> command;
+    private final Optional<String> message;
 
-    public AbstractGameEvent(String command, String message) {
+    /**
+     * Creates a new AbstractGameEvent with the specified command and message.
+     *
+     * @param command The command to execute when the event is triggered.
+     * @param message The message to broadcast when the event is triggered.
+     */
+    public AbstractGameEvent(Optional<String> command, Optional<String> message) {
         this.command = command;
         this.message = message;
     }
 
     protected void executeCommand() {
-        if (command != null && !command.isEmpty()) {
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+        if (command.isPresent()) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), String.valueOf(command));
         }
     }
 
     protected void broadcastMessage() {
-        if (message != null && !message.isEmpty()) {
-            Bukkit.getServer().broadcastMessage(message);
+        if (message.isPresent()) {
+            Bukkit.getServer().broadcastMessage(String.valueOf(message));
         }
     }
 }
