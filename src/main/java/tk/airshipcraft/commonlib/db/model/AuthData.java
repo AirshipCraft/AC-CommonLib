843530f4edb348816830484ba2e5a0bb4982110d
zeyad rashed <brwnboy2002@gmail.com>
2024-01-06 17:44:39 -0500

    feat: add equal and hashcode methods

--- src/main/java/tk/airshipcraft/commonlib/db/model/AuthData.java
+++ src/main/java/tk/airshipcraft/commonlib/db/model/AuthData.java
@@ -3,9 +3,28 @@ package tk.airshipcraft.commonlib.db.model;
 import java.util.UUID;
 
 /**
- * Represents the authentication data for a player, storing information related to the linkage of Minecraft accounts to Discord accounts.
- * This class is used to verify if a player has successfully linked their accounts.
+ * <p>Represents the authentication data for a player, storing information related to the linkage of Minecraft accounts to Discord accounts.
+ * This class is used to verify if a player has successfully linked their accounts.</p>
  *
+ * <p>Authentication data is stored in a database table with the following schema: </p>
+ * <pre>
+ *     CREATE TABLE IF NOT EXISTS auth_data (
+ *     id UUID PRIMARY KEY,
+ *     IGN VARCHAR(255) NOT NULL,
+ *     username VARCHAR(255) NOT NULL,
+ *     token UUID NOT NULL,
+ *     verified BOOLEAN NOT NULL
+ *     );
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_IGN_index ON auth_data (IGN);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_username_index ON auth_data (username);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_token_index ON auth_data (token);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_verified_index ON auth_data (verified);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_IGN_username_index ON auth_data (IGN, username);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_IGN_token_index ON auth_data (IGN, token);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_IGN_verified_index ON auth_data (IGN, verified);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_username_token_index ON auth_data (username, token);
+ *     CREATE UNIQUE INDEX IF NOT EXISTS auth_data_username_verified_index ON auth_data (username, verified);
+ * </pre>
  * @author notzune
  * @version 1.0.0
  * @since 2024-01-05
@@ -123,5 +142,45 @@ public class AuthData {
      */
     public void setVerified(boolean verified) {
         this.verified = verified;
+    }
+
+    /**
+     * Returns a String representation of the AuthData object.
+     *
+     * @return A String representation of the AuthData object.
+     */
+    @Override
+    public String toString() {
+        return "AuthData{" +
+                "id=" + id +
+                ", IGN='" + IGN + '\'' +
+                ", username='" + username + '\'' +
+                ", token=" + token +
+                ", verified=" + verified +
+                '}';
+    }
+
+    /**
+     * Checks if the specified object is equal to this AuthData object.
+     *
+     * @param obj The object to compare to.
+     * @return True if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof AuthData other) {
+            return this.id.equals(other.id) && this.IGN.equals(other.IGN) && this.username.equals(other.username) && this.token.equals(other.token) && this.verified == other.verified;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the hash code of the AuthData object.
+     *
+     * @return An integer representing the hash code.
+     */
+    @Override
+    public int hashCode() {
+        return this.id.hashCode() + this.IGN.hashCode() + this.username.hashCode() + this.token.hashCode() + Boolean.hashCode(this.verified);
     }
 }
